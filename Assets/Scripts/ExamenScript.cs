7a72313e48a99e5739566a63e23e10b158ebdaaf
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-04-19 08:12:54 -0300

    Initial commit

---
+++ Assets/Scripts/ExamenScript.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ExamenScript : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# ExamenJeroPrimerBimSegundaVez
+ 
