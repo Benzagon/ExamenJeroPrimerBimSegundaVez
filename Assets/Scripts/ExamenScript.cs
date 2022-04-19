940cb48eea9136e862dd3eba9b169f8f30b718c4
Benzagon <91753390+Benzagon@users.noreply.github.com>
2022-04-19 08:27:46 -0300

    ExamFinished

--- Assets/Scripts/ExamenScript.cs
+++ Assets/Scripts/ExamenScript.cs
@@ -4,13 +4,37 @@ using UnityEngine;
 
 public class ExamenScript : MonoBehaviour
 {
+    public string codGusto;
+    public float gramos;
+
     void Start()
     {
-        
-    }
+        if (gramos < 250 || gramos > 3000)
+        {
+            Debug.Log("Porfavor ingrese una cantidad de gramos válida");
+        }
+        else
+        {
+            switch (codGusto)
+            {
+                case "CHO":
+                    Debug.Log("El monto total es de $"+ gramos / 1000 * 500);
+                    break;
 
-    void Update()
-    {
-        
+                case "DDL":
+                    Debug.Log("El monto totat es de $" + gramos / 1000 * 500);
+                    break;
+
+                case "FRU":
+                    float monto = gramos / 1000 * 500;
+                    Debug.Log("El monto totat es de $" + (monto - (monto / 100 * 10)));
+                    break;
+
+                default:
+                    Debug.Log("Porfavor ingrese un código de gusto válido");
+                    break;
+            }
+        }
     }
+      
 }
